b6f9a9bc21d561de280a36d298091c27d3e9f0b5
Matthew-19 <113844755+Matthew-19@users.noreply.github.com>
2024-04-02 15:21:46 +0200

    Update upcomingmeetings.tsx

--- pages/home/meetings/upcomingmeetings.tsx
+++ pages/home/meetings/upcomingmeetings.tsx
@@ -49,6 +49,51 @@ export default function UpcomingMeetings() {
             </MeetingCard>
           </MeetingCardsContainer>
         </DateMeetingsContainer>
+        
+        <DateMeetingsContainer>
+          <MeetingDate>3 Jun. 2024</MeetingDate>
+
+          <MeetingCardsContainer>
+            <MeetingCard>
+              <MeetingCard.Info>
+                <ProfilePicture
+                  border={true}
+                  size={88}
+                  src={"/images/woman.png"}
+                />
+                <MeetingCard.TitleOwnerContainer>
+                  <MeetingCard.Title>ERP Requirements</MeetingCard.Title>
+                  <MeetingCard.Owner>your</MeetingCard.Owner>
+                </MeetingCard.TitleOwnerContainer>
+              </MeetingCard.Info>
+
+              <MeetingCard.Duration>
+                From 10:00 AM To 11:00 AM
+              </MeetingCard.Duration>
+
+              <MeetingCard.Button>Join meeting</MeetingCard.Button>
+            </MeetingCard>
+            <MeetingCard>
+              <MeetingCard.Info>
+                <ProfilePicture
+                  border={true}
+                  size={88}
+                  src={"/images/woman.png"}
+                />
+                <MeetingCard.TitleOwnerContainer>
+                  <MeetingCard.Title>ERP Requirements</MeetingCard.Title>
+                  <MeetingCard.Owner>your</MeetingCard.Owner>
+                </MeetingCard.TitleOwnerContainer>
+              </MeetingCard.Info>
+
+              <MeetingCard.Duration>
+                From 7:00 AM To 9:00 AM
+              </MeetingCard.Duration>
+
+              <MeetingCard.Button>Join meeting</MeetingCard.Button>
+            </MeetingCard>
+          </MeetingCardsContainer>
+        </DateMeetingsContainer>
       </AllMeetingsContainer>
     </>
   );
